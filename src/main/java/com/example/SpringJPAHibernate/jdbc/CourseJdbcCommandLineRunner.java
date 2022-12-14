aded712f48002fb80183a897e47d24afce958e64
ARUNDHATI <arundhati@ARUNDHATIs-MacBook-Pro.local>
2022-12-14 12:00:22 +0530

    changes

---
+++ src/main/java/com/example/SpringJPAHibernate/jdbc/CourseJdbcCommandLineRunner.java
@@ -0,0 +1,22 @@
+package com.example.SpringJPAHibernate.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CourseJdbcCommandLineRunner implements CommandLineRunner {
+
+	@Autowired
+	private CourseJdbcRepository cjr;
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		cjr.insert();
+		
+	}
+	
+	
+
+}
